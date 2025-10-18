cea9a21834e54d6d17817c2afdfbfe2a4f36bfe2
mugenryuu <rfyamazaki@outlook.com>
2025-10-18 14:13:11 +0800

    Updated for sonarqube coverage

---
+++ __mocks__/@react-native-async-storage/async-storage.js
@@ -0,0 +1,7 @@
+export default {
+  getItem: jest.fn(() => Promise.resolve(null)),
+  setItem: jest.fn(() => Promise.resolve()),
+  removeItem: jest.fn(() => Promise.resolve()),
+  clear: jest.fn(() => Promise.resolve()),
+  getAllKeys: jest.fn(() => Promise.resolve([])),
+};
